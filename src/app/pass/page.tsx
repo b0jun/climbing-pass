840f92c7bd92690e2410e13babd19b370c953051
b0jun <bjkim0228@naver.com>
2023-10-20 16:41:01 +0900

    수정: 언어 선택기 추가

--- src/app/pass/page.tsx
+++ src/app/pass/page.tsx
@@ -1,31 +1,44 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+import { LANGUAGE } from '../constants/lang';
+import LanguageSelector from '../components/LanguageSelector';
 
-export default function Pass() {
+const Pass = () => {
+	const searchParams = useSearchParams();
+	const lang = searchParams.get('lang');
+	const isEng = lang === 'en';
+	const currentLang = isEng ? 'EN' : 'KO';
+
 	return (
 		<main className="flex flex-col items-center justify-center py-10">
+			<LanguageSelector />
 			<div className="relative w-36 h-36 mb-14">
 				<Image src="/images/logo.png" alt="logo" fill />
 			</div>
 			<Link
 				href={{
 					pathname: '/register',
-					query: { pass: 'day-pass' },
+					query: { pass: 'day-pass', ...(lang && { lang }) },
 				}}
 				className="w-56 h-32 bg-blue-200 flex flex-col justify-center items-center rounded-md mb-20 shadow-lg"
 			>
-				<h3 className="text-2xl">일일이용</h3>
+				<h3 className="text-2xl">{LANGUAGE[currentLang].dayPass1}</h3>
 			</Link>
 			<Link
 				href={{
 					pathname: '/register',
-					query: { pass: 'first-pass' },
+					query: { pass: 'first-visit', ...(lang && { lang }) },
 				}}
 				className="w-56 h-32 bg-blue-200 flex flex-col justify-center items-center rounded-md shadow-lg"
 			>
-				<h3 className="text-2xl">일일체험</h3>
-				<p>(첫 이용자)</p>
+				<h3 className="text-2xl">{LANGUAGE[currentLang].dayPass2}</h3>
+				<p>({LANGUAGE[currentLang].firstVisit})</p>
 			</Link>
 		</main>
 	);
-}
+};
+
+export default Pass;
