945db5026e68b2f6fb288412b08e3716fca9bf7b
b0jun <bjkim0228@naver.com>
2023-12-06 18:00:39 +0900

    수정: 완료 시 메인 버튼 추가

--- src/app/[locale]/[gym]/complete/layout.tsx
+++ src/app/[locale]/[gym]/complete/layout.tsx
@@ -1,7 +1,7 @@
 export default async function Layout(props: { children: React.ReactNode }) {
 	return (
 		<main className="flex flex-col max-w-xl min-h-full mx-auto shadow-mobile">
-			<div className="flex items-center justify-center flex-1 bg-[#f2f2f2]">
+			<div className="flex items-center justify-center flex-1 bg-contents">
 				{props.children}
 			</div>
 		</main>
--- src/app/[locale]/[gym]/complete/page.tsx
+++ src/app/[locale]/[gym]/complete/page.tsx
@@ -1,11 +1,25 @@
+'use client';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { useTranslations } from 'next-intl';
 
-const Complete = () => {
+import Button from '@/components/Button';
+
+type Props = {
+	params: {
+		gym: string;
+	};
+};
+
+const Complete = ({ params: { gym } }: Props) => {
 	const t = useTranslations('Complete');
+	const router = useRouter();
+	const goToMain = () => {
+		router.replace(`/${gym}/pass`);
+	};
 
 	return (
-		<div className="flex flex-col items-center px-4 text-center">
+		<div className="flex flex-col items-center px-4 text-center w-full">
 			<Image
 				src="/images/complete.png"
 				width={128}
@@ -15,6 +29,9 @@ const Complete = () => {
 			/>
 			<h3 className="text-xl my-3 font-bold">{t('titlePass')}</h3>
 			<h4 className="text-base">{t('description')}</h4>
+			<div className="mt-5 w-[250px]">
+				<Button label={t('goToMain')} onClick={goToMain} />
+			</div>
 		</div>
 	);
 };
