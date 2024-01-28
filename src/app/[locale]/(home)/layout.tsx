2b3bf935952f6029d6238fc67b3394b0bd60335e
b0jun <bjkim0228@naver.com>
2024-01-28 21:17:12 +0900

    수정: 홈 추가

--- src/app/[locale]/(home)/layout.tsx
+++ src/app/[locale]/(home)/layout.tsx
@@ -1,7 +1,7 @@
 export default async function Layout(props: { children: React.ReactNode }) {
 	return (
 		<main className="max-w-xl flex flex-col min-h-full mx-auto shadow-mobile">
-			<div className="flex-1 flex justify-center items-center bg-red-200">
+			<div className="flex-1 flex justify-center items-center bg-contents">
 				{props.children}
 			</div>
 		</main>
--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -1,7 +1,87 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const GYM_DUMMY = [
+	{
+		id: 1,
+		name: '1호점',
+		domain: 'one',
+	},
+	{
+		id: 2,
+		name: '아주 아주 긴이름을 가진 지점',
+		domain: 'two',
+	},
+];
+
 const Home = () => {
+	const isLogin = true;
 	return (
 		<section>
-			<div className="text-6xl">🚧 관리자 전용 페이지</div>
+			<h3 className="text-[20px] mb-4 font-bold ml-2">나의 지점 리스트</h3>
+			<div className="border border-black p-5 rounded-md">
+				{isLogin ? (
+					<ul className="flex flex-col gap-2">
+						{GYM_DUMMY.map(({ id, name, domain }) => (
+							<li
+								key={id}
+								className="flex items-center border-b pb-2 last:border-0 last:pb-0"
+							>
+								<h3 className="font-bold w-[250px]">{name}</h3>
+								<Link
+									href={`/${domain}/pass`}
+									className="bg-[#7F8487] text-white rounded-md py-2 px-4 mr-2"
+								>
+									패스
+								</Link>
+								<Link
+									href={`/${domain}/manager`}
+									className="bg-[#413F42] text-white rounded-md py-2 px-4"
+								>
+									관리
+								</Link>
+							</li>
+						))}
+					</ul>
+				) : (
+					<div className="relative">
+						<ul className="flex flex-col gap-2 blur">
+							<li className="flex items-center border-b pb-2">
+								<h3 className="font-bold w-[250px]">1호점</h3>
+								<div className="bg-[#7F8487] text-white rounded-md py-2 px-4 mr-2">
+									패스
+								</div>
+								<div className="bg-[#413F42] text-white rounded-md py-2 px-4">
+									관리
+								</div>
+							</li>
+							<li className="flex items-center">
+								<h3 className="font-bold w-[250px]">2호점</h3>
+								<div className="bg-[#7F8487] text-white rounded-md py-2 px-4 mr-2">
+									패스
+								</div>
+								<div className="bg-[#413F42] text-white rounded-md py-2 px-4">
+									관리
+								</div>
+							</li>
+						</ul>
+						<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+							<Link
+								href="/login"
+								className="border rounded-md border-black bg-stone-300/60 flex py-2 pl-2 pr-1 gap-1"
+							>
+								<h4>로그인 해주세요</h4>
+								<Image
+									src="icons/ic_fill_right.svg"
+									width={24}
+									height={24}
+									alt=""
+								/>
+							</Link>
+						</div>
+					</div>
+				)}
+			</div>
 		</section>
 	);
 };
