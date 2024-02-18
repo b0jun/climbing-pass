a8b088e4b867de3de42a466daf15b68bd25e1b1e
b0jun <bjkim0228@naver.com>
2024-02-18 21:03:02 +0900

    수정: 홈화면 수정 및 404 처리

--- src/app/[gym]/layout.tsx
+++ src/app/[gym]/layout.tsx
@@ -1,3 +1,10 @@
+import GymWhiteList from '@/components/GymWhiteList';
+
 export default async function Layout(props: { children: React.ReactNode }) {
-	return <>{props.children}</>;
+	return (
+		<>
+			{props.children}
+			<GymWhiteList />
+		</>
+	);
 }
