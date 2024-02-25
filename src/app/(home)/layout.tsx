c61d3a6d869182bf77fd59958453f95065f6362a
b0jun <bjkim0228@naver.com>
2024-02-25 16:30:12 +0900

    수정: (홈) 폴더 삭제

--- src/app/(home)/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Layout(props: { children: React.ReactNode }) {
-	return (
-		<main className="max-w-xl flex flex-col min-h-full mx-auto shadow-mobile">
-			<div className="flex-1 flex justify-center items-center bg-contents">
-				{props.children}
-			</div>
-		</main>
-	);
-}
