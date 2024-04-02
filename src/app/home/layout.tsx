50d005be7c4d2bd4610a116b579d7124709d1cb6
b0jun <bjkim0228@naver.com>
2024-04-02 09:32:10 +0900

    추가: 로그아웃 추가 및 기타 버튼 수정

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,9 +1,10 @@
+import Header from './Header';
+
 export default async function Layout(props: { children: React.ReactNode }) {
 	return (
 		<main className="max-w-xl flex flex-col min-h-full mx-auto shadow-mobile">
-			<div className="flex-1 flex justify-center items-center bg-contents">
-				{props.children}
-			</div>
+			<Header />
+			<div className="flex-1 flex justify-center items-center bg-contents">{props.children}</div>
 		</main>
 	);
 }
