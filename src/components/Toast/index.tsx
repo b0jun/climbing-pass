8c042f45061f1b1a47563f522da580f67ea5a588
b0jun <bjkim0228@naver.com>
2024-05-01 15:48:58 +0900

    수정: 토스트 위치 수정

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -3,7 +3,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Toast = () => {
-	return <ToastContainer hideProgressBar autoClose={1500} draggable={false} closeOnClick />;
+	return <ToastContainer hideProgressBar autoClose={1500} draggable={false} closeOnClick position="bottom-right" />;
 };
 
 export default Toast;
