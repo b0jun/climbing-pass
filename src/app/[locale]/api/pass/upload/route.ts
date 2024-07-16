e3132b7ca7482a04709427241e01441921a58ba7
b0jun <bjkim0228@naver.com>
2024-07-16 16:21:17 +0900

    수정: blob 콘솔 제거

--- src/app/[locale]/api/pass/upload/route.ts
+++ src/app/[locale]/api/pass/upload/route.ts
@@ -29,7 +29,7 @@ export async function POST(request: Request): Promise<NextResponse> {
 				// ⚠️ This will not work on `localhost` websites,
 				// Use ngrok or similar to get the full upload flow
 
-				console.log('blob upload completed', blob, tokenPayload);
+				// console.log('blob upload completed', blob, tokenPayload);
 
 				try {
 					// Run any logic after the file upload completed
