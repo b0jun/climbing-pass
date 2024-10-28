f17664ab2ad838c49ec29cdf40428ddd816063da
b0jun <bjkim0228@naver.com>
2024-10-28 20:50:02 +0900

    mod: Analytics 주석

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css';
-import { Analytics } from '@vercel/analytics/react';
+// import { Analytics } from '@vercel/analytics/react';
 import { Noto_Sans, Noto_Sans_KR } from 'next/font/google';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
@@ -15,41 +15,41 @@ const notoSansKr = Noto_Sans_KR({ subsets: ['latin'], weight: ['400', '700', '90
 const notoSans = Noto_Sans({ subsets: ['latin'], weight: ['400', '700', '900'] });
 
 export const metadata: Metadata = {
-	title: 'Pass',
+  title: 'Pass',
 };
 
 export const viewport: Viewport = {
-	width: 'device-width',
-	initialScale: 1,
-	maximumScale: 1,
-	userScalable: false,
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default async function RootLayout({
-	children,
-	params: { locale },
+  children,
+  params: { locale },
 }: {
-	children: React.ReactNode;
-	params: { locale: string };
+  children: React.ReactNode;
+  params: { locale: string };
 }) {
-	const messages = await getMessages();
+  const messages = await getMessages();
 
-	return (
-		<html lang={locale}>
-			<head>
-				<link rel="icon" href="/favicon.ico" sizes="any" />
-			</head>
-			<body className={`${notoSansKr.className} ${notoSans.className}`}>
-				<ReactQuery>
-					<NextAuth>
-						<NextIntlClientProvider messages={messages}>
-							<Providers>{children}</Providers>
-						</NextIntlClientProvider>
-					</NextAuth>
-				</ReactQuery>
-				{/* <Analytics /> */}
-				<Toast />
-			</body>
-		</html>
-	);
+  return (
+    <html lang={locale}>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
+      <body className={`${notoSansKr.className} ${notoSans.className}`}>
+        <ReactQuery>
+          <NextAuth>
+            <NextIntlClientProvider messages={messages}>
+              <Providers>{children}</Providers>
+            </NextIntlClientProvider>
+          </NextAuth>
+        </ReactQuery>
+        {/* <Analytics /> */}
+        <Toast />
+      </body>
+    </html>
+  );
 }
