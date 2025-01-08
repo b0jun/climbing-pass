c13716d7f8ae0cc89cfc2f20bf5defd73cac6f74
b0jun <bjkim0228@naver.com>
2025-01-08 20:52:28 +0900

    mod: 총 방문횟수 추가
    - pass 필드내 총 방문 이력 갯수 visitCounts 추가

--- src/app/[locale]/api/gym/[gym]/passList/route.ts
+++ src/app/[locale]/api/gym/[gym]/passList/route.ts
@@ -8,87 +8,111 @@ import prisma from '@/lib/prisma';
 const secret = process.env.NEXTAUTH_SECRET;
 
 const GET = async (request: NextRequest, context: any) => {
-	try {
-		const token = await getToken({ req: request, secret });
-		const userData = verifyJwt(token?.accessToken as string);
-		const { gym } = context.params;
+  try {
+    const token = await getToken({ req: request, secret });
+    const userData = verifyJwt(token?.accessToken as string);
+    const { gym } = context.params;
 
-		if (!userData) {
-			return NextResponse.json(
-				{ errorMessage: 'No Auth' },
-				{
-					status: 401,
-				}
-			);
-		}
-		const gymData = await prisma.gym.findFirst({
-			where: {
-				domain: gym,
-				userId: userData.id,
-			},
-			select: {
-				name: true,
-			},
-		});
+    if (!userData) {
+      return NextResponse.json(
+        { errorMessage: 'No Auth' },
+        {
+          status: 401,
+        }
+      );
+    }
+    const gymData = await prisma.gym.findFirst({
+      where: {
+        domain: gym,
+        userId: userData.id,
+      },
+      select: {
+        name: true,
+      },
+    });
 
-		if (!gymData) {
-			return NextResponse.json(
-				{ errorMessage: 'No Permission' },
-				{
-					status: 403,
-				}
-			);
-		}
+    if (!gymData) {
+      return NextResponse.json(
+        { errorMessage: 'No Permission' },
+        {
+          status: 403,
+        }
+      );
+    }
 
-		const searchParams = request.nextUrl.searchParams;
-		const passDate = searchParams.get('passDate') ?? dayjs().format('YYYY/MM/DD');
-		const passType = searchParams.get('passType');
+    const searchParams = request.nextUrl.searchParams;
+    const passDate = searchParams.get('passDate') ?? dayjs().format('YYYY/MM/DD');
+    const passType = searchParams.get('passType');
 
-		const { id } = userData;
+    const { id } = userData;
 
-		const nextDate = new Date(passDate);
-		nextDate.setDate(nextDate.getDate() + 1);
+    const nextDate = new Date(passDate);
+    nextDate.setDate(nextDate.getDate() + 1);
 
-		const passData = await prisma.pass.findMany({
-			where: {
-				userId: id,
-				gymId: gym,
-				createdAt: {
-					gte: new Date(passDate),
-					lt: new Date(nextDate),
-				},
-				...(passType && { type: passType as 'DayPass' | 'DayExperience' }),
-				NOT: {
-					status: 'DELETED',
-				},
-			},
-			select: {
-				id: true,
-				name: true,
-				phoneNumber: true,
-				dateOfBirth: true,
-				createdAt: true,
-				type: true,
-				status: true,
-				shoesRental: true,
-			},
-			orderBy: {
-				createdAt: 'desc',
-			},
-		});
+    const passData = await prisma.pass.findMany({
+      where: {
+        userId: id,
+        gymId: gym,
+        createdAt: {
+          gte: new Date(passDate),
+          lt: new Date(nextDate),
+        },
+        ...(passType && { type: passType as 'DayPass' | 'DayExperience' }),
+        NOT: {
+          status: 'DELETED',
+        },
+      },
+      select: {
+        id: true,
+        name: true,
+        phoneNumber: true,
+        dateOfBirth: true,
+        createdAt: true,
+        type: true,
+        status: true,
+        shoesRental: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
 
-		return NextResponse.json(
-			{
-				gymName: gymData ? gymData.name : '',
-				passList: passData ? passData : [],
-			},
-			{
-				status: 200,
-			}
-		);
-	} catch (e: any) {
-		return NextResponse.json({ message: e.message }, { status: e.code });
-	}
+    const visitCounts = await prisma.pass.groupBy({
+      by: ['name', 'phoneNumber'],
+      where: {
+        userId: id,
+        gymId: gym,
+        NOT: {
+          status: 'DELETED',
+        },
+      },
+      _count: {
+        _all: true,
+      },
+    });
+    const visitCountsMap = Object.fromEntries(
+      visitCounts.map((v) => [`${v.name}-${v.phoneNumber}`, v._count._all - 1])
+    );
+    const passDataWithCounts = passData.map((pass) => {
+      const key = `${pass.name}-${pass.phoneNumber}`;
+      return {
+        ...pass,
+        totalVisits: visitCountsMap[key] || 0, // 전체 데이터 기준 방문 횟수 추가
+      };
+    });
+
+    return NextResponse.json(
+      {
+        gymName: gymData ? gymData.name : '',
+        passList: passData ? passDataWithCounts : [],
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (e: any) {
+    return NextResponse.json({ message: e.message }, { status: e.code });
+  }
 };
 
 export { GET };
