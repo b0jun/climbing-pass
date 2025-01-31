a27e52db3265eb5a40d2b1c12b924ded6b3cece4
b0jun <bjkim0228@naver.com>
2025-01-31 17:14:13 +0900

    feat: next svgr 세팅

--- next.config.js
+++ next.config.js
@@ -4,16 +4,23 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false,
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'rgb3lh2iesbgvjl4.public.blob.vercel-storage.com',
-				pathname: '**',
-			},
-		],
-	},
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rgb3lh2iesbgvjl4.public.blob.vercel-storage.com',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 module.exports = withNextIntl(nextConfig);
