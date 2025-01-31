7eeb0e5c4f85ac5af901def5b0509b592091effc
b0jun <bjkim0228@naver.com>
2025-01-31 17:14:28 +0900

    feat: svg 아이콘 추가

--- src/components/SVG/index.ts
+++ src/components/SVG/index.ts
@@ -1,7 +1,8 @@
 import ClimbingShoesIcon from './ClimbingShoesIcon';
 import EditIcon from './EditIcon';
+import MenuIcon from './menu.svg';
+import CloseIcon from './close.svg';
 import PassDeleteIcon from './PassDeleteIcon';
 import PassViewIcon from './PassViewIcon';
 import RefreshIcon from './RefreshIcon';
-
-export { ClimbingShoesIcon, EditIcon, PassDeleteIcon, PassViewIcon, RefreshIcon };
+export { ClimbingShoesIcon, EditIcon, MenuIcon, CloseIcon, PassDeleteIcon, PassViewIcon, RefreshIcon };
