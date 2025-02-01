319f75cb39ea4044481027a94e8a1a4104062c84
b0jun <bjkim0228@naver.com>
2025-02-01 14:50:42 +0900

    mod: tailwind config screens 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,34 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-	content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
-	theme: {
-		screens: {
-			xs: '290px',
-		},
-		extend: {
-			boxShadow: {
-				mobile: '0 0 20px rgba(130,130,130,0.15)',
-			},
-			height: {
-				withoutHeader: 'calc(100% - 48px)',
-			},
-			colors: {
-				darkBlue: '#2F4861',
-				lightBlue: '#afc5db',
-				extraDarkBlue: '#2B4075',
-				extraLightBlue: '#dbe1f0',
-				main: '#3f72af',
-				error: '#BE3144',
-				contents: '#f5f5f5',
-				form: '#fefefe',
-			},
-			animation: {
-				sheetUp: 'bottom-sheet-up 200ms ease-in-out',
-				sheetDown: 'bottom-sheet-down 200ms ease-in-out',
-			},
-		},
-	},
-	plugins: [require('@tailwindcss/forms')],
+  content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      screens: {
+        xs: '290px',
+      },
+      boxShadow: {
+        mobile: '0 0 20px rgba(130,130,130,0.15)',
+      },
+      height: {
+        withoutHeader: 'calc(100% - 48px)',
+      },
+      colors: {
+        darkBlue: '#2F4861',
+        lightBlue: '#afc5db',
+        extraDarkBlue: '#2B4075',
+        extraLightBlue: '#dbe1f0',
+        main: '#3f72af',
+        error: '#BE3144',
+        contents: '#f5f5f5',
+        form: '#fefefe',
+      },
+      animation: {
+        sheetUp: 'bottom-sheet-up 200ms ease-in-out',
+        sheetDown: 'bottom-sheet-down 200ms ease-in-out',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
 };
 export default config;
