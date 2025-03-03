884db1f7f8b83311aa668a2b1f8c53919125e772
b0jun <bjkim0228@naver.com>
2025-03-03 16:38:57 +0900

    refactor: NextAuth(v4) -> Auth.js(v5) 마이그레이션
    - 로그인 내부 서버 로직으로 수정
      - 기존) 클라이언트 서버 -> API Route 방식에서 서버 컴포넌트에서 직접 로그인 구성
    - v5 마이그레이션
      - auth.ts, auth의 route 파일 v5 구조에 맞게 수정
      - 에러핸들링 추가할때 이슈로 인해 서버액션 추가로 적용(https://stackoverflow.com/questions/78627862/next-auth-signin-return-errorconfiguration-in-responce-for-invalid-credenti)

--- src/app/[locale]/[gym]/pass/[type]/consent/page.tsx
+++ src/app/[locale]/[gym]/pass/[type]/consent/page.tsx
@@ -1,28 +1,28 @@
 import ConsentForm from './components/ConsentForm';
 
 async function fetchConsentInfo() {
-	try {
-		const res = await fetch(`${process.env.NEXTAUTH_URL}/api/gym/dolmenge_jeonpo/consentInfo`, {
-			cache: 'no-cache',
-		});
-		if (!res.ok) {
-			throw new Error('Failed to fetch consent information');
-		}
-		const data = await res.json();
-		return data;
-	} catch (error) {
-		console.error(error);
-		return { message: 'Error fetching consent information' };
-	}
+  try {
+    const res = await fetch(`${process.env.API_URL}/api/gym/dolmenge_jeonpo/consentInfo`, {
+      cache: 'no-cache',
+    });
+    if (!res.ok) {
+      throw new Error('Failed to fetch consent information');
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return { message: 'Error fetching consent information' };
+  }
 }
 
 const Consent = async () => {
-	const gymData = await fetchConsentInfo();
-	return (
-		<section className="flex flex-col justify-center">
-			<ConsentForm gymData={gymData} />
-		</section>
-	);
+  const gymData = await fetchConsentInfo();
+  return (
+    <section className="flex flex-col justify-center">
+      <ConsentForm gymData={gymData} />
+    </section>
+  );
 };
 
 export default Consent;
--- src/app/[locale]/api/pass/update/route.ts
+++ src/app/[locale]/api/pass/update/route.ts
@@ -4,64 +4,64 @@ import { getToken } from 'next-auth/jwt';
 import { verifyJwt } from '@/lib/jwt';
 import prisma from '@/lib/prisma';
 
-const secret = process.env.NEXTAUTH_SECRET;
+const secret = process.env.AUTH_SECRET;
 
 const PATCH = async (request: NextRequest) => {
-	try {
-		const token = await getToken({ req: request, secret });
-		const userData = verifyJwt(token?.accessToken as string);
+  try {
+    const token = await getToken({ req: request, secret });
+    const userData = verifyJwt(token?.accessToken as string);
 
-		if (!userData) {
-			return NextResponse.json(
-				{ errorMessage: 'No Auth' },
-				{
-					status: 401,
-				}
-			);
-		}
+    if (!userData) {
+      return NextResponse.json(
+        { errorMessage: 'No Auth' },
+        {
+          status: 401,
+        },
+      );
+    }
 
-		const gymData = await prisma.gym.findFirst({
-			where: {
-				userId: userData.id,
-			},
-		});
+    const gymData = await prisma.gym.findFirst({
+      where: {
+        userId: userData.id,
+      },
+    });
 
-		if (!gymData) {
-			return NextResponse.json(
-				{ errorMessage: 'No Permission' },
-				{
-					status: 403,
-				}
-			);
-		}
+    if (!gymData) {
+      return NextResponse.json(
+        { errorMessage: 'No Permission' },
+        {
+          status: 403,
+        },
+      );
+    }
 
-		const { body } = await request.json();
-		const { id, shoesRental, type } = body;
+    const { body } = await request.json();
+    const { id, shoesRental, type } = body;
 
-		await prisma.pass.update({
-			where: {
-				id,
-			},
-			data: {
-				shoesRental,
-				type,
-			},
-		});
+    await prisma.pass.update({
+      where: {
+        id,
+      },
+      data: {
+        shoesRental,
+        type,
+      },
+    });
 
-		return NextResponse.json(
-			{ message: 'success' },
-			{
-				status: 201,
-			}
-		);
-	} catch (e: any) {
-		return NextResponse.json(
-			{ message: e.message },
-			{
-				status: 500,
-			}
-		);
-	}
+    return NextResponse.json(
+      { message: 'success' },
+      {
+        status: 201,
+      },
+    );
+  } catch (e: any) {
+    return NextResponse.json(
+      { message: e.message },
+      {
+        status: 500,
+      },
+    );
+  }
 };
 
 export { PATCH };
