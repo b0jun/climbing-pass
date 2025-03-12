efc382115d0fc949169a013d5917f1c1538b84c6
b0jun <bjkim0228@naver.com>
2025-03-12 21:14:46 +0900

    refactor: next-intl 구조 수정
    - Next Auth v5 버전과 호환성 좋은 구조로 수정

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-import { Pathnames, LocalePrefix } from 'next-intl/routing';
-
-export const pathnames: Pathnames<typeof locales> = {
-	'/': '/',
-};
-export const defaultLocale = 'ko' as const;
-export const locales = ['ko', 'en'] as const;
-export const localePrefix: LocalePrefix<typeof locales> = 'as-needed';
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,60 @@
+import { NextRequest, NextResponse } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
 
-import { localePrefix, defaultLocale, locales, pathnames } from './config';
+import { auth } from '@/auth';
 
-export default createMiddleware({
-	locales,
-	defaultLocale,
-	localePrefix,
-	pathnames,
+import { locales, routing } from './i18n/routing';
+
+const intlMiddleware = createMiddleware(routing);
+
+const publicRoutes = ['/'];
+
+const authRoutes = ['/login'];
+
+const testPathnameRegex = (pages: string[], pathName: string): boolean => {
+  // Replace dynamic routes with regex
+  const pathsWithParams = pages.map((p) => p.replace(/\[.*?\]/g, '[^/]+'));
+
+  return RegExp(
+    `^(/(${locales.join('|')}))?(${pathsWithParams.flatMap((p) => (p === '/' ? ['', '/'] : p)).join('|')})/?$`,
+    'i',
+  ).test(pathName);
+};
+
+const authMiddleware = auth((req) => {
+  const isAuthPage = testPathnameRegex(authRoutes, req.nextUrl.pathname);
+  const isLogged = !!req.auth;
+
+  // Redirect to login page if not authenticated
+  if (!isLogged && !isAuthPage) {
+    return NextResponse.redirect(new URL('/login', req.nextUrl));
+  }
+
+  // Redirect to home page if authenticated and trying to access auth pages
+  if (isLogged && isAuthPage) {
+    return NextResponse.redirect(new URL('/', req.nextUrl));
+  }
+
+  return intlMiddleware(req);
 });
 
-export const config = {
-	matcher: [
-		// Enable a redirect to a matching locale at the root
-		'/',
+const middleware = (req: NextRequest) => {
+  const isPublicPage = testPathnameRegex(publicRoutes, req.nextUrl.pathname);
+  const isAuthPage = testPathnameRegex(authRoutes, req.nextUrl.pathname);
 
-		// Set a cookie to remember the previous locale for
-		// all requests that have a locale prefix
-		'/en/:path*',
+  if (isAuthPage) {
+    return (authMiddleware as any)(req);
+  }
 
-		// Enable redirects that add missing locales
-		// (e.g. `/pathnames` -> `/en/pathnames`)
-		'/((?!_next|_vercel|.*\\..*).*)',
-	],
+  if (isPublicPage) {
+    return intlMiddleware(req);
+  } else {
+    return (authMiddleware as any)(req);
+  }
 };
+
+export const config = {
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
+};
+
+export default middleware;
