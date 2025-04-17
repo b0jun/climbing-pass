0a9f11bfbb4764c5bb0ffd103453adda88cd264e
b0jun <bjkim0228@naver.com>
2025-04-17 13:19:38 +0900

    mod: GetPassList SQL 수정
    - 조건에 따라 그룹핑 수정 이름 + 전화번호, 이름 + 생년월일
    - status: EXPIRED추가에 따른 조건 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
       <div className="flex h-full w-full items-center justify-center">
         <div className="flex flex-col items-center gap-4 rounded-2xl bg-form p-20">
           <p className="text-lg">지원하지 않는 페이지입니다.</p>
-          <Link href="/" className="w-[100px] rounded-md bg-gray-300 px-3 py-1 text-white hover:bg-gray-400">
+          <Link href="/" className="rounded-md bg-gray-300 px-3 py-1 text-white hover:bg-gray-400">
             홈으로 가기
           </Link>
         </div>
